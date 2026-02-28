dad6472530a401d30047e3b868f33f095c128df0
Taha <taha@Tahas-MacBook-Pro.local>
2026-02-28 22:52:03 +0800

    adding support for apple machines (mac) for gui rendering

--- workspace/so101/src/main.cpp
+++ workspace/so101/src/main.cpp
@@ -2,14 +2,16 @@
 #include "openmanip/Gui.hpp"
 #include "openmanip/logger.hpp"
 #include <Eigen/src/Core/Matrix.h>
+#include <filesystem>
 #include <iostream>
 
 static Logger logger;
 
 int main(int argc, char** argv){
     openmanip::RobotSystem robot;
-    std::string modelPath_xml = "/home/ambi/OpenManip/workspace/models/SO101/scene.xml";
-    std::string modelPath_urdf = "/home/ambi/OpenManip/workspace/models/SO101/so101_new_calib.urdf";
+    std::filesystem::path root(PROJECT_ROOT);
+    std::string modelPath_xml = (root / "workspace/models/SO101/scene.xml").string();
+    std::string modelPath_urdf = (root / "workspace/models/SO101/so101_new_calib.urdf").string();
     if (!robot.initialize(modelPath_xml, modelPath_urdf)){
         logger.error() << "Failed to load model";
         return 1;
